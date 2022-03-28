a1c25addeb58517e6eeb172534f089c1de5ace0a
AnamQaiser <101175112+AnamQaiser@users.noreply.github.com>
2022-03-28 19:48:44 +0500

    Update page15.kt

--- page15.kt
+++ page15.kt
@@ -17,7 +17,7 @@ class page15 : AppCompatActivity() {
         setContentView(R.layout.activity_page15)
     }
     fun sendMessage(view: View) {
-        val editText = findViewById<EditText>(R.id.editTextTextPersonName)
+        val editText = findViewById<EditText>(R.id.editTextTextalphabets)
         val message = editText.text.toString()
         val intent = Intent(this, DisplayMessageActivity::class.java).apply {
             putExtra(EXTRA_MESSAGE, message)
